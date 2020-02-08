e687292173dc441cf9ebf0db06a6cc155cc75e1d
Olivia <oliviapower.02@gmail.com>
2020-02-08 02:28:14 +0000

    update routes

--- routes/event.js
+++ routes/event.js
@@ -16,8 +16,11 @@ module.exports = (db) => {
   });
 
   router.get('/:id/dates', (req,res) => {
-    //add queries
-    res.render(dates.ejs)
+    //is passeed event_id
+    //add creator dates to db queries
+    //get event title and display!
+    let templateVars = {};
+    res.render('dates');
   });
 
   router.get('/:id/poll/:auth', (req,res) => {
@@ -30,10 +33,18 @@ module.exports = (db) => {
   });
 
   router.post('/add', (req, res) =>{
-    res.redirect(`/${1}/dates`);
+    //add event to database
+    //retrive event_id from database and redirct with that id
+    let event_id;
+    db.query(``)
+    .then(res => event_id = res.row[0].id)
+    res.redirect(`/${event_id}/dates`);
   });
 
   router.post('/users', (req, res) =>{
+    //add users to database
+    //Send creator to poll page??
+    //res.redirect('poll');
     res.send(req.body);
   });
 
