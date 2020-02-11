5fe82ca444fdf3b69f6da400f38ddda03420ec48
Olivia <oliviapower.02@gmail.com>
2020-02-11 01:43:52 +0000

    add style

--- routes/event.js
+++ routes/event.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router  = express.Router();
-const { addEvent, addDate, addUserGuest, getIdFromEmail } = require('../lib/queries.js');
+const { addEvent, addDate, addUserGuest, getIdFromEmail, getStartEnd } = require('../lib/queries.js');
 const nodemailer = require('nodemailer');
 const { sendMail, transporter } = require('../nodemailer/mailFunctions')
 
 module.exports = (db) => {
 
-  router.get('/:id/poll/:auth', (req,res) => {
+  router.get('/poll/:auth', (req,res) => {
+    let auth = req.params.auth;
+
     res.render('poll.ejs');
 
   });
@@ -69,45 +71,11 @@ module.exports = (db) => {
         console.log(email, user_id);
         sendMail(email, user_id)
       })
+
     });
-
   });
-
 
   return router;
 };
 
 
-
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'midterm.labber@gmail.com',
-//     pass: 'labber123'
-//   }
-// });
-
-
-// let user1 = {
-//   name: 'Matthew',
-//   email: 'm.zj.chan@gmail.com',
-//   id: '9de12b3b-1075-4b4b-be0e-27de280913f4'
-// }
-
-// const sendMail = function(user) {
-//   var mailOptions = {
-//     from: 'midterm.labber@gmail.com',
-//     to: user.email,
-//     subject: "You've been invited to an event!",
-//     text: `Hi ${user.name},
-//     You've been invited to an event! please follow this link to update your availibity
-//     localhost:8080/${user.id}`
-//   };
-//   transporter.sendMail(mailOptions, function(error, info){
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log('Email sent: ' + info.response);
-//     }
-//   });
-// }
