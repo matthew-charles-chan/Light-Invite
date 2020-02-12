8380f834d6fed962c0e74f7ce7bd73b5d71c4a85
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-02-12 02:26:09 +0000

    add title/ description to datepick

--- lib/test.sql
+++ lib/test.sql
@@ -93,7 +93,20 @@
 --   WHERE users.id = '5a5dd1d0-64d7-4670-9626-e04aef5baece';
 
 
-SELECT count(*)
-from votes
-JOIN users on user_id = users.id
-where event_id =  'a6e2fce2-abae-4e63-9a29-5ca2c78a9ffd';
+-- SELECT count(*)
+-- from votes
+-- JOIN users on user_id = users.id
+-- where event_id =  'a6e2fce2-abae-4e63-9a29-5ca2c78a9ffd';
+
+SELECT date_id,
+      to_char(start_time, 'Mon DD YYYY  HH24:MI') as start_time,
+      to_char(start_time + (select duration from events where events.id = dates.event_id) * Interval '1 minute', 'Mon DD YYYY  HH24:MI') as end_time,
+      sum(case when isAvailable = true then 1 else 0 end) as yes_count,
+      sum(case when isAvailable = false then 1 else 0 end) as no_count,
+      title,
+      events.description
+      from votes
+      JOIN dates on date_id = dates.id
+      JOIN events on events.id = event_id
+      where dates.id = 14
+      GROUP BY date_id, dates.id, title, events.description;
